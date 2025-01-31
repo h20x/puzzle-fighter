ed299892d6d4ae06d434a692ec37e2c13f133621
John <dmt.chmk@gmail.com>
2025-01-31 16:00:43 +0200

    Add controls

--- src/field.js
+++ src/field.js
@@ -1,7 +1,7 @@
 export class Field {
-  constructor(width, height, parent) {
-    this._width = width;
-    this._height = height;
+  constructor(cols, rows, parent) {
+    this._cols = cols;
+    this._rows = rows;
     this._parent = parent;
     this._field = null;
   }
@@ -10,8 +10,8 @@ export class Field {
     if (!this._field) {
       this._field = document.createElement('div');
       this._field.classList.add('field');
-      this._field.style.setProperty('--width', this._width);
-      this._field.style.setProperty('--height', this._height);
+      this._field.style.setProperty('--cols', this._cols);
+      this._field.style.setProperty('--rows', this._rows);
       this._parent.append(this._field);
     }
 
@@ -19,18 +19,30 @@ export class Field {
   }
 
   _createHTML(state) {
-    const len = this._width * this._height;
+    const len = this._cols * this._rows;
     let html = '';
 
     for (let i = 0; i < len; ++i) {
       if (state && state[i]) {
-        // gem
-        html += `<div class="cell"></div>`;
+        html += `<div class="cell">${this._createGem(state[i].type())}</div>`;
       } else {
         html += `<div class="cell"></div>`;
       }
     }
 
     return html;
+  }
+
+  _createGem(type) {
+    const cls = ['gem'];
+    cls.push(type.toLowerCase());
+
+    if (type === 'r' || type === 'g' || type === 'b') {
+      cls.push('crash');
+    } else {
+      cls.push('simple');
+    }
+
+    return `<div class="${cls.join(' ')}"></div>`;
   }
 }
